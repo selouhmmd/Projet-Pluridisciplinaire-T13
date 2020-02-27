11d6a4d3b2d81b5451801c35a9cc232f9a845b0a
ZairiAimenDz <42500714+ZairiAimenDz@users.noreply.github.com>
2020-02-27 17:37:51 +0100

    Book Model

---
+++ ESISBA/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESISBA.Models
+{
+    // Must Be Modified After Knowing How To Work With APIs
+    class Book
+    {
+        public string title { get; set; }
+        public string writer { get; set; }
+        public string description { get; set; }
+        public int nbr { get; set; }
+        public int available { get; set; }
+        public string coverurl { get; set; }
+    }
+}
