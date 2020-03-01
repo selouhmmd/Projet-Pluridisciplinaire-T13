2a9b56ed83af81d3dea063aec25815249ba2909a
ZairiAimenDz <42500714+ZairiAimenDz@users.noreply.github.com>
2020-03-01 19:00:04 +0100

    Some More Modifications

--- ESISBA/MainPage.xaml.cs
+++ ESISBA/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using ESISBA.Models;
+using System.Collections.ObjectModel;
 
 namespace ESISBA
 {
@@ -14,17 +15,52 @@ namespace ESISBA
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+
+        private ObservableCollection<Book> _books;
+
+        internal ObservableCollection<Book> Books { get => _books; set => _books = value; }
+
         public MainPage()
         {
             InitializeComponent();
-            var bkList = new List<Book>
+            _books = new ObservableCollection<Book>
             {
                  new Book {Title="Random Book Title",Description="this is a random description,so let's try to write a big paragraph so it can seem like we care",Writer="Random",Nbr=1,Available=1},
                  new Book {Title="Random Title",Description="this is a random description,so let's try to write a big paragraph so it can seem like we care",Writer="Random",Nbr=1,Available=1},
                  new Book{Title="Title Random Book",Description="this is a random description,so let's try to write a big paragraph so it can seem like we care",Writer="Random",Nbr=1,Available=1},
                  new Book{Title="Book Random Title",Description="this is a random description,so let's try to write a big paragraph so it can seem like we care",Writer="Danrom",Nbr=1,Available=1}
             };
-            bookList.ItemsSource = bkList;
+            bookList.ItemsSource = _books;
+        }
+
+        private void BookList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        { 
+            bookList.SelectedItem = null;
+        }
+
+
+        private void FavClicked(object sender, EventArgs e)
+        {
+            //var mitem = sender as MenuItem;
+            //Book bk = mitem.CommandParameter as Book;
+
+        }
+        private void DeleteClicked(object sender, EventArgs e)
+        {
+            var bk = (sender as MenuItem).CommandParameter as Book;
+            _books.Remove(bk);
+        }
+
+        private void BookList_Refreshing(object sender, EventArgs e)
+        {
+            // Check The Google Sheets Element
+            bookList.EndRefresh();
+        }
+
+        private void bookList_ItemTapped_1(object sender, ItemTappedEventArgs e)
+        {var book = e.Item as Book;
+            DisplayAlert("Selected", book.Title, "Yes");
+
         }
     }
 }
---
+++ ESISBA/Models/BookList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESISBA.Models
+{
+    class BookList : List<Book>
+    {
+        public string Title { get; set; }
+        public string ShortTitle { get; set; }
+        public BookList(string title , string shortTitle)
+        {
+            this.Title = title;
+            this.ShortTitle = shortTitle;
+        }
+    }
+}
