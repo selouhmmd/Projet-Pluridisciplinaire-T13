4e604d22eb7007c6d70db5b1e78c8f4754717c0d
ZairiAimenDz <42500714+ZairiAimenDz@users.noreply.github.com>
2020-03-03 13:42:52 +0100

    A Bit More Modifications

--- ESISBA/View/MainPage.xaml.cs
+++ ESISBA/View/MainPage.xaml.cs
@@ -20,7 +20,7 @@ namespace ESISBA
             {
                 List<Book> books = new List<Book>
                 {
-                     new Book{Title="Learning Java",Description="this is a random description,so let's try to write a big paragraph so it can seem like we care",Writer="Random",Nbr=1,Available=1},
+                     new Book{Title="Learning Java",Description="this Called is a random description,so let's try to write a big paragraph so it can seem like we care",Writer="Random",Nbr=1,Available=1},
                      new Book{Title="Programming With C#",Description="this is a random description,so let's try to write a big paragraph so it can seem like we care",Writer="Random",Nbr=1,Available=1},
                      new Book{Title="Android Studio For Beginners",Description="this is a random description,so let's try to write a big paragraph so it can seem like we care",Writer="Random",Nbr=1,Available=1},
                      new Book{Title="Machine Learning And AI",Description="this is a random description,so let's try to write a big paragraph so it can seem like we care",Writer="Danrom",Nbr=1,Available=1},
@@ -42,7 +42,7 @@ namespace ESISBA
                     return books;
                 }
 
-                return books.Where(c => c.Title.ToLower().Contains(searchtxt.ToLower()));
+                return books.Where(c => (c.Title.ToLower().Contains(searchtxt.ToLower()) || c.Description.ToLower().Contains(searchtxt.ToLower())));
             }
         private ObservableCollection<Book> _books;
 
@@ -67,12 +67,6 @@ namespace ESISBA
         {
             //var mitem = sender as MenuItem;
             //Book bk = mitem.CommandParameter as Book;
-
-        }
-        private void DeleteClicked(object sender, EventArgs e)
-        {
-            var bk = (sender as MenuItem).CommandParameter as Book;
-            _books.Remove(bk);
         }
 
         private void BookList_Refreshing(object sender, EventArgs e)
