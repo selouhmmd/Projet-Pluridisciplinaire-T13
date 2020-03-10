47e12dd99cf48f1816ca3c41cbc9f43ba8597840
ZairiAimenDz <42500714+ZairiAimenDz@users.noreply.github.com>
2020-03-10 11:19:18 +0100

    More Changes

--- ESISBA/View/SearchPage.xaml.cs
+++ ESISBA/View/SearchPage.xaml.cs
@@ -16,5 +16,10 @@ namespace ESISBA.View
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
     }
 }
